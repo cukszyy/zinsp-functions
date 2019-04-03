35d73341e4e8d971810600a518a1f21ff68ea987
luucasportella <lucas_portellasantos@yahoo.com.br>
2019-04-03 00:27:27 -0300

    first commit

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'scream text',
+            createdAt: '2019-04-03T01:30:19.900Z',
+            likeCount: 5,
+            commentCount: 2,
+        }
+    ]
+}
